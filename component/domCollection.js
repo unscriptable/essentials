6e78cd9c71d5b8bd5779d104583aa71a930446ea
unscriptable <john@unscriptable.com>
2018-04-05 16:33:27 -0400

    Fix types for patch adapters.

--- component/domCollection.js
+++ component/domCollection.js
@@ -1,33 +1,42 @@
+//@flow
 
 export const domCollection
-    = (container, childPrototype, push) => {
-        // Remove childPrototype from document?
-        // if (container.ownerDocument.contains(childPrototype))
-        //     childPrototype.parentNode.removeChild(childPrototype)
+    : CreateAdapter<Object,Element,Element>
+    = (container, childPrototype, _update) => {
 
         const create
             = data => {
                 const child = childPrototype.cloneNode(true)
-                return update(child, data)
+                return (update(child, data):any)
             }
 
         const update
             = (child, data) => {
-                push(child, data)
-                return child
+                _update(child, data)
+                return (child:any)
             }
 
         const insert
             = (pos, child) => {
                 const sibling = container.children[pos] || null
-                return container.insertBefore(child, sibling)
+                return (container.insertBefore(child, sibling):any)
             }
 
         const remove
             = pos => {
                 const child = container.children[pos]
-                return child && container.removeChild(child)
+                return (child && container.removeChild(child):any)
             }
 
         return { create, insert, remove, update }
     }
+
+type Adapter<T,E>
+    = {
+        create: T => E,
+        insert: (number, E) => E,
+        remove: number => ?E,
+        update: (E, T) => E
+    }
+type CreateAdapter<T,E,P>
+    = (P, E, (E,T)=>any) => Adapter<T,E>
