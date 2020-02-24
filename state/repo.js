84459e37d2257e172e6edf0611b7420cb51e4d87
John Hann <john@paceline.fit>
2020-02-24 08:36:48 -0500

    Improve types and documentation comments.

--- state/repo.js
+++ state/repo.js
@@ -2,22 +2,28 @@
 
 // Create a state repository.  Accepts a clone function, then an initial state.
 // A state repository uses patterns much like a reduce function.  Repository
-// functions produce functions that store or retrieve state as side-effects
+// functions store or retrieve state.  The storage operations are side-effects
 // and pass the input value through to the output.  This makes them well suited
-// to storing state in promise chains.  State is typically an object.
+// to storing state in promise chains.  State is typically an object, but only
+// the assign function requires state to be an object.  The merge and view
+// function are generic.
+//
 // merge((currentState, value) => newState) - Provide a state mutation function
-//   (accepts the current state and a value and returns a new state) and get
-//   back an identity function (passes the input through to the output) that
-//   saves the state returned from the state mutation function as a side effect.
-// assign((currentState, value) => newProperties) - Provide a state mutation
-//   function (accepts the current state and a value and returns new properties)
-//   and get back an identity function (passes the input through to the output)
-//   that saves new state properties returned from the state mutation function
-//   as a side effect.
-// view((currentState, value) => f(currentState, value)) - Works like merge,
-//   but the provided function doesn't mutate the state.
+//   and get back an identity function.  The state mutation function must return
+//   a new state, which is applied as a side effect.  In a promise chain, the
+//   fulfillment value is passed through to the next `.then()`.
+// assign(value => newProperties) - Provide a state mutation function and get
+//   back an identity function.  The state mutation function must return an
+//   object with properties that will be assigned to the state (which also must
+//   be an object) as a side-effect.  In a promise chain, the fulfillment value
+//   is passed through to the next `.then()`.
+// view((currentState, value) => newValue) - Provide a function that will use
+//   the final state.  The function is passed the state and the promise chain's
+//   current fulfillment value and must return a value for the next `.then()`.
+//   Note: unlike the other functions, view does not execute as a side effect.
+//
 // Example usage:
-// const { merge, view } = repo(cloneSimple, {})
+// const { merge, assign, view } = repo(cloneSimple, {})
 // getX()
 //   .then(merge((state, x) => ({ ...state, x })))
 //   .then(transformToY) // receives x
@@ -30,7 +36,7 @@ export const repo
 
     const merge = f => value => ( state = clone(f(state, value)), value )
     const assign
-      = f => value => ( state = { ...state, ...clone(f(state, value)) }, value )
+      = f => value => ( state = { ...state, ...clone(f(value)) }, value )
     const view = f => value => f(clone(state), value)
 
     return { merge, view }
--- state/stack.js
+++ state/stack.js
@@ -2,20 +2,25 @@
 
 // Create a state stack.  Accepts a clone function, then an initial item to put
 // on the stack. The stack manages state by exposing stack-like functions.
-// The stack functions mutate the stack as side effects, returning the value
+// The push function mutates the stack as a side effect, returning the value
 // stored on the stack.  A state stack is well-suited to promise chains where
-// lots of items are accumulated and then required all at once.
+// lots of items are accumulated and then required in a specific order.
+//
 // push(state) - Save a state item onto a stack.
 // pop() - Remove and return the most recently pushed state item.
 // items() - Return all of the state items as an array and empty the stack.
+//
 // Example usage:
+// const { push, items } = stack(simpleClone)()
 // getThing()
 //   .then(push)
 //   .then(getAnotherThing) // receives pushed item / result of getThing()
 //   .then(push)
 //   .then(items) // extracts all pushed items
 //   .then(things => doSomething(...things))
+//
 // Example usage:
+// const { push, items } = stack(simpleClone)()
 // push(getThing())
 //   .then(push(getAnotherThing)) // receives pushed item / result of getThing()
 //   .then(items) // extracts all pushed items
